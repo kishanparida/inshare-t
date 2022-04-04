56d27e51bbebc40ccb75b3925cc11d3ef8f4dc4f
kishanparida <64662032+kishanparida@users.noreply.github.com>
2022-04-04 14:33:17 +0530

    add cors

--- src/server.js
+++ src/server.js
@@ -3,12 +3,12 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
-// const cors = require('cors');
-// // Cors
-// const corsOptions = {
-//   origin: process.env.ALLOWED_CLIENTS.split(',')
-//   // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
-// }
+const cors = require('cors');
+// Cors
+const corsOptions = {
+  origin: process.env.ALLOWED_CLIENTS.split(',')
+  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+}
 //
 // // Default configuration looks like
 // // {
@@ -18,7 +18,7 @@ const path = require('path');
 // //     "optionsSuccessStatus": 204
 // //   }
 //
-// app.use(cors(corsOptions))
+app.use(cors(corsOptions))
 const static_path=path.join(__dirname,"../public");
 const template_path=path.join(__dirname,"../views");
 app.use(express.static('public'));
